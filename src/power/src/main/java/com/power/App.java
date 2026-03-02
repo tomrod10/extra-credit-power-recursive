7b2ada22906643be05f87832387dfe99040aa7da
Tomás <63356102+tomrod10@users.noreply.github.com>
2026-03-01 20:33:28 -0800

    bump junit ver to 5.10, write function, write test

--- src/power/src/main/java/com/power/App.java
+++ src/power/src/main/java/com/power/App.java
@@ -2,17 +2,18 @@ package com.power;
 
 public class App {
     public static void main(String[] args) {
-        double twoSq = power(2, 2);
-        double twoPointThree = power(2.3, 4);
-
-        System.out.printf("2^2 = %f\n", twoSq); // Expect 4
-        System.out.printf("2.3^4 = %f\n", twoPointThree); // Expect 27.9841
+        System.out.println("Power recursive functions");
     }
 
     public static double power(double base, int exponent) {
         if (exponent == 1) {
             return base;
         }
+
+        if (exponent == 0) {
+            return 1;
+        }
+
         return base * power(base, exponent - 1);
     }
 }
--- src/power/src/test/java/com/power/AppTest.java
+++ src/power/src/test/java/com/power/AppTest.java
@@ -1,20 +1,29 @@
 package com.power;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
- * Unit test for simple App.
+ * Unit test for recursive power function.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+    // @Test
+    // public void testPower() {
+    // assertEquals(2 * 2, App.power(2, 2), 0.001);
+    // assertEquals(2.3 * 2.3 * 2.3 * 2.3, App.power(2.3, 4), 0.001);
+    // }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 2, 4.0",
+            "2, 3, 8.0",
+            "5, 0, 1.0",
+            "0, 5, 0.0",
+            "2.3, 4, 27.9841"
+    })
+    public void testPower(double base, int exponent, double expected) {
+        assertEquals(expected, App.power(base, exponent), 0.001);
     }
 }
