d2ee39db31e032363d7d5c0405eaa38749704c7a
Tomás <63356102+tomrod10@users.noreply.github.com>
2026-03-01 19:03:20 -0800

    write recursive power function

--- src/power/src/main/java/com/power/App.java
+++ src/power/src/main/java/com/power/App.java
@@ -8,4 +8,11 @@ public class App {
         System.out.printf("2^2 = %f\n", twoSq); // Expect 4
         System.out.printf("2.3^4 = %f\n", twoPointThree); // Expect 27.9841
     }
+
+    public static double power(double base, int exponent) {
+        if (exponent == 1) {
+            return base;
+        }
+        return base * power(base, exponent - 1);
+    }
 }
