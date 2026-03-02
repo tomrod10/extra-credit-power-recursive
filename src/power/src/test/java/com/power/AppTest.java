2c1aaaa03ce53d146baf770714bcaa36663752f1
Tomás <63356102+tomrod10@users.noreply.github.com>
2026-03-01 20:37:53 -0800

    remove commented code and add more test cases

--- src/power/src/test/java/com/power/AppTest.java
+++ src/power/src/test/java/com/power/AppTest.java
@@ -9,18 +9,14 @@ import org.junit.jupiter.params.provider.CsvSource;
  * Unit test for recursive power function.
  */
 public class AppTest {
-    // @Test
-    // public void testPower() {
-    // assertEquals(2 * 2, App.power(2, 2), 0.001);
-    // assertEquals(2.3 * 2.3 * 2.3 * 2.3, App.power(2.3, 4), 0.001);
-    // }
-
     @ParameterizedTest
     @CsvSource({
             "2, 2, 4.0",
             "2, 3, 8.0",
             "5, 0, 1.0",
             "0, 5, 0.0",
+            "-2, 2, 4.0",
+            "-2, 3, -8.0",
             "2.3, 4, 27.9841"
     })
     public void testPower(double base, int exponent, double expected) {
